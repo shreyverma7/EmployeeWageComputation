b4670b6ae71941ca4a22094dd6eb02d773aa2cc3
Shrey Kumar Verma <shreykumar.v@tvsnext.com>
2023-07-11 11:19:21 +0530

    [Shrey] Add: Implemented employee Daily wages feature

--- EmployeeWageComputation/EmployeeAttendance.cs
+++ EmployeeWageComputation/EmployeeAttendance.cs
@@ -7,7 +7,9 @@ using System.Threading.Tasks;
 namespace EmployeeWageComputation
 {
     internal class EmployeeAttendance
+
     {
+        const int WAGE_PER_HR = 20 , WAGE_PER_DAY = 8 ;
         public void EmpAttendance() {
             Random random = new Random();
             int empCheck = random.Next(0,2);
@@ -15,6 +17,11 @@ namespace EmployeeWageComputation
                 Console.WriteLine("Employee Is Present");
             else
                 Console.WriteLine("Employee Is Absent");
+        }
+        public void CalculateEmpWage() {
+            int dailyEmpwage = WAGE_PER_DAY * WAGE_PER_HR;
+            Console.WriteLine(dailyEmpwage);
+
         }
     }
 }
