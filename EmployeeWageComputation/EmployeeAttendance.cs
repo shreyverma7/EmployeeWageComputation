135605e1cce8b077c51965ba17117d5015a118fb
Shrey Kumar Verma <shreykumar.v@tvsnext.com>
2023-07-12 10:30:49 +0530

    [Shrey] Add : Adding total working hour wages function

--- EmployeeWageComputation/EmployeeAttendance.cs
+++ EmployeeWageComputation/EmployeeAttendance.cs
@@ -10,7 +10,7 @@ namespace EmployeeWageComputation
     internal class EmployeeAttendance
 
     {
-        const int WAGE_PER_HR = 20 , FULL_DAY_HR = 8 , PART_DAY_HR = 4, IS_FULL_TIME =0 , IS_PART_TIME = 1 , TOTAL_WORKING_DAYS = 20;
+        const int WAGE_PER_HR = 20 , FULL_DAY_HR = 8 , PART_DAY_HR = 4, IS_FULL_TIME =0 , IS_PART_TIME = 1 ,TOTAL_WORKING_HRS =100, TOTAL_WORKING_DAYS = 20;
         int totalEmpWage = 0 ,empHrs = 0 ;
         Random random = new Random();
         public void EmpAttendance() {
@@ -35,7 +35,7 @@ namespace EmployeeWageComputation
 
              user case 3 is above one is
             */
-            for (int day = 0; day < TOTAL_WORKING_DAYS; day++)
+            for (int day = 0; day < TOTAL_WORKING_DAYS  &&  empHrs < TOTAL_WORKING_HRS; day++)
             {
                 int empCheck = random.Next(0, 3);//0 1 2
                 switch (empCheck)
