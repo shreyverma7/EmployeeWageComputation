84985f816b2f5e5587517ce2a6c29f79cbdd56a1
Shrey Kumar Verma <shreykumar.v@tvsnext.com>
2023-07-12 10:21:07 +0530

    [Shrey] Add : Adding month wages function

--- EmployeeWageComputation/EmployeeAttendance.cs
+++ EmployeeWageComputation/EmployeeAttendance.cs
@@ -10,7 +10,7 @@ namespace EmployeeWageComputation
     internal class EmployeeAttendance
 
     {
-        const int WAGE_PER_HR = 20 , FULL_DAY_HR = 8 , PART_DAY_HR = 4, IS_FULL_TIME =0 , IS_PART_TIME = 1;
+        const int WAGE_PER_HR = 20 , FULL_DAY_HR = 8 , PART_DAY_HR = 4, IS_FULL_TIME =0 , IS_PART_TIME = 1 , TOTAL_WORKING_DAYS = 20;
         int totalEmpWage = 0 ,empHrs = 0 ;
         Random random = new Random();
         public void EmpAttendance() {
@@ -22,36 +22,38 @@ namespace EmployeeWageComputation
                 Console.WriteLine("Employee Is Absent");
         }
         public void CalculateEmpWage() {
-            int empCheck = random.Next(0, 3);//0 1 2
-           /* if (empCheck == IS_FULL_TIME)
+            
+            /* if (empCheck == IS_FULL_TIME)
+             {
+                 empHrs = FULL_DAY_HR;
+             }
+
+             if(empCheck == IS_PART_TIME)
+             {
+                 empHrs = PART_DAY_HR;
+             }
+
+             user case 3 is above one is
+            */
+            for (int day = 0; day < TOTAL_WORKING_DAYS; day++)
             {
-                empHrs = FULL_DAY_HR;
+                int empCheck = random.Next(0, 3);//0 1 2
+                switch (empCheck)
+                {
+                    case IS_FULL_TIME:
+                        empHrs += FULL_DAY_HR;
+                        break;
+
+                    case IS_PART_TIME:
+                        empHrs += PART_DAY_HR;
+                        break;
+                    default:
+                        empHrs += 0;
+                        break;
+
+
+                }
             }
-                
-            if(empCheck == IS_PART_TIME)
-            {
-                empHrs = PART_DAY_HR;
-            }
-
-            user case 3 is above one is
-           */  
-
-            switch (empCheck)
-            {
-                case IS_FULL_TIME:
-                    empHrs = FULL_DAY_HR;
-                    break;
-
-                case IS_PART_TIME:
-                    empHrs = PART_DAY_HR;
-                    break;
-                default:
-                    empHrs = 0;
-                    break;
-
-
-            }
-
 
             int totalEmpWage = empHrs * WAGE_PER_HR;
             Console.WriteLine(totalEmpWage);
